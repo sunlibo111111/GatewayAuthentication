fa33aed2ca112417787fa6b58e1a04e48fa69d3e
sunlibo111111 <sunliboyy@126.com>
2019-11-19 13:53:44 +0800

    Update Program.cs
    增加端口，与consul配置对应

--- ServiceA/Program.cs
+++ ServiceA/Program.cs
@@ -12,6 +12,6 @@ namespace ServiceA
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://localhost:8011");
     }
 }
